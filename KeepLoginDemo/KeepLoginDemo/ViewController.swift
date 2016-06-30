771c64f57c234efd97a84fc7360c2f2bc092d1a1
RamboLu <coderambolu@gmail.com>
2016-06-30 16:19:56 +0800

    keepdemo init

---
+++ KeepLoginDemo/KeepLoginDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeepLoginDemo
+//
+//  Created by 路政浩 on 16/6/28.
+//  Copyright © 2016年 RamboLu. All rights reserved.
+//
+
+import UIKit
+import AssetsLibrary
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
